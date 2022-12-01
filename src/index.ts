b0161d6d9b3558cd56f004e78b0677fb6300436b
Saroj Shrestha <saroj.shr911@gmail.com>
2022-12-01 22:16:20 +0545

    trying for ws

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { l } from './utils/logger';
-
-const port = process.env.PORT ?? 8000;
-
 const aedes = require('aedes')();
 const server = require('aedes-server-factory').createServer(aedes, {
-    trustProxy: true
+  ws: true,
 });
+const port = 1883;
 
 server.listen(port, function () {
-    l.info('Server up and running on port %s', port);
+  console.log('server started and listening on port ', port);
 });
